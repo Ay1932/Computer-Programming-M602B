f9334e9163e106013482d7ce821883b53010529d
Your Name <pandavayush004@gmail.com>
2024-10-15 17:02:44 +0200

    Computer Programming

---
+++ Hello.py
@@ -0,0 +1,8 @@
+# print the welcome msg
+print ('Hello World!')
+
+#Ask the user for the name
+user_name = input("What is your name?")
+
+#Print the user_name with welcome msg
+print("Welcome " + user_name + " to the Python programming!")
---
+++ Hello_World.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string user_name;
+    cout << "What is your name?" << std::endl;
+    cin >> user_name;
+    cout << "welcome " << user_name << " to Python programming!" << std::endl;
+
+    return 0;
+}
