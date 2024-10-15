01a31cb2359794406d8d11b28a26fe92de591cfd
Your Name <pandavayush004@gmail.com>
2024-10-15 17:14:55 +0200

    Computer Programming

--- Hello.py
+++ Hello.py
@@ -6,3 +6,9 @@ user_name = input("What is your name?")
 
 #Print the user_name with welcome msg
 print("Welcome " + user_name + " to the Python programming!")
+
+#Ask for the surname
+user_surname = input("What is your surname?")
+
+#print the full name with welcome msg
+print ("Welcome " + user_name + user_surname + " to the Python programming!")
