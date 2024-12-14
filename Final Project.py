9cb210f58cc4636e0f17f12fe9e62bcdac63b384
Ay1932 <ayushpandav01@gmail.com>
2024-12-14 12:17:22 +0100

    process

--- Final Project.py
+++ Final Project.py
@@ -10,4 +10,10 @@ class CarbonCalculator
         """calculate energy emission through equation"""
         return((electricity * 12 * 0.0005) + (gas * 12 * 0.0053) + (fuel * 12 * 2.32))
     
-    def calc_waste()
+    def calc_waste(waste , recycling)
+        """Calculate waste emission through equation"""
+        return(waste * 12 * (0.57 - recycling / 100))
+    
+    def calc_travel(traveled_distance , fuel_efficiency)
+        """Calculate Business Travel emission through equation"""
+        return(traveled_distance * (1 / fuel_efficiency) * 2.31)
